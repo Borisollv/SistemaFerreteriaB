ba1513101595686024f096a38c307d06b6e8ee06
Borisllv <borisomarllv64@gmail.com>
2025-01-03 21:31:21 -0600

    bm

--- CapaEntidades/Empleado.cs
+++ CapaEntidades/Empleado.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-    namespace CapaEntidades
+namespace CapaEntidades
+{
+    public class Empleado
     {
-        public class Empleado
-        {
-            public int idEmpleado { get; set; }
-            public String nombres { get; set; }
-            public String apellidos { get; set; }
-            public int dni { get; set; }
-            public String correo { get; set; }
-            public String celular { get; set; }
-            public String puesto { get; set; }
-            public String usuario { get; set; }
-            public String contrasena { get; set; }
-            public Boolean estado { get; set; }
-        }
+        public int idEmpleado { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public int dni { get; set; }
+        public string correo { get; set; }
+        public string celular { get; set; }
+        public string puesto { get; set; }
+        public string usuario { get; set; }
+        public string contrasena { get; set; }
+        public bool estado { get; set; }
     }
+}
